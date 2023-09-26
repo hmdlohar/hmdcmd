d51a651672311c28ce992e405b5327da870e9abf
hyper2 <hmdlohar@gmail.com>
2023-09-26 16:45:15 +0530

    init commit

---
+++ src/git/cloneAndAssignGitWithKey.js
@@ -0,0 +1,6 @@
+
+// git use specific key
+// git config core.sshCommand "ssh -i ./ssh/mlit_rsa -F /dev/null"
+
+// git clone with specific key
+// GIT_SSH_COMMAND='ssh -i ./rw-boa' git clone git@github.com:hmdlohar/rw-backoffice-app.git
